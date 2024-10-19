e94d685ed1676f3ddd5e9a65d696eea22df44c42
PRAVEEN200307 <crouselearn@gmail.com>
2024-10-19 10:55:19 +0530

    FEAT: added a cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import router from './routes/routes.js';
 import { connectDB } from './lib/db.js';
+import cors from 'cors'
 
 const app = express();
 const SERVER_PORT = 5000;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
